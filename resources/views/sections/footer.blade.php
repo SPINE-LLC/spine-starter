26b34aac22624e29bd64ed7f5bec1e78fffe725b
Jon Brockett <18336484+jonbrockett@users.noreply.github.com>
2025-12-08 15:00:44 -0500

    add footer navs

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,3 +1,17 @@
-<footer class="content-info">
+<footer id="colophon" class="content-info">
   @php(dynamic_sidebar('sidebar-footer'))
+
+  @if (has_nav_menu('footer_navigation'))
+    <nav class="nav-footer" aria-label="{{ wp_get_nav_menu_name('footer_navigation') }}">
+      {!! \App\footer_nav() !!}
+    </nav>
+  @endif
+
+  @if (has_nav_menu('privacy_navigation'))
+    <nav class="nav-privacy" aria-label="{{ wp_get_nav_menu_name('privacy_navigation') }}">
+      {!! \App\privacy_nav() !!}
+    </nav>
+  @endif
+
+  <p class="copyright"><small>&copy; {{ wp_date('Y') }} {{ $siteName }}</small></p>
 </footer>
