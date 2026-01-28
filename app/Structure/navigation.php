8d5f9df8ea867472afe45ee75a0756d4025ac67e
Jon Brockett <18336484+jonbrockett@users.noreply.github.com>
2026-01-28 11:21:50 -0500

    add transient caching to menus

--- app/Structure/navigation.php
+++ app/Structure/navigation.php
@@ -23,18 +23,39 @@ add_action('after_setup_theme', function () {
 });
 
 /**
+ * Clear menu caches when menus are updated.
+ *
+ * @return void
+ */
+add_action('wp_update_nav_menu', function () {
+    delete_transient('primary_nav_' . get_current_blog_id());
+    delete_transient('primary_btns_nav_' . get_current_blog_id());
+    delete_transient('compact_nav_' . get_current_blog_id());
+    delete_transient('compact_btns_nav_' . get_current_blog_id());
+    delete_transient('footer_nav_' . get_current_blog_id());
+    delete_transient('privacy_nav_' . get_current_blog_id());
+});
+
+/**
  * Primary navigation menu function.
  *
  * @return string
  */
 function primary_nav() {
-    return wp_nav_menu([
+    $cache_key = 'primary_nav_' . get_current_blog_id();
+    $cached = get_transient($cache_key);
+    if ($cached !== false) {
+        return $cached;
+    }
+    $output = wp_nav_menu([
         'theme_location' => 'primary_navigation',
         'menu_class' => 'nav is-dropdown',
         'depth' => 3,
         'echo' => false,
         'walker' => new \App\Walker\Aria_Walker_Nav_Menu()
     ]);
+    set_transient($cache_key, $output, HOUR_IN_SECONDS);
+    return $output;
 }
 
 /**
@@ -43,13 +64,20 @@ function primary_nav() {
  * @return string
  */
 function primary_btns_nav() {
-	return wp_nav_menu([
-		'theme_location' => 'primary_buttons',
-		'menu_class' => 'nav button-nav wp-block-buttons',
-		'depth' => 1,
-		'echo' => false,
-		'walker' => new \App\Walker\Button_Walker_Nav_Menu()
-	]);
+    $cache_key = 'primary_btns_nav_' . get_current_blog_id();
+    $cached = get_transient($cache_key);
+    if ($cached !== false) {
+        return $cached;
+    }
+    $output = wp_nav_menu([
+        'theme_location' => 'primary_buttons',
+        'menu_class' => 'nav button-nav wp-block-buttons',
+        'depth' => 1,
+        'echo' => false,
+        'walker' => new \App\Walker\Button_Walker_Nav_Menu()
+    ]);
+    set_transient($cache_key, $output, HOUR_IN_SECONDS);
+    return $output;
 }
 
 /**
@@ -58,13 +86,20 @@ function primary_btns_nav() {
  * @return string
  */
 function compact_btns_nav() {
-	return wp_nav_menu(array(
-		'theme_location' => 'compact_buttons',
-		'menu_class' => 'nav button-nav wp-block-buttons',
-		'depth' => 1,
-		'echo' => false,
-		'walker' => new \App\Walker\Button_Walker_Nav_Menu()
-	));
+    $cache_key = 'compact_btns_nav_' . get_current_blog_id();
+    $cached = get_transient($cache_key);
+    if ($cached !== false) {
+        return $cached;
+    }
+    $output = wp_nav_menu(array(
+        'theme_location' => 'compact_buttons',
+        'menu_class' => 'nav button-nav wp-block-buttons',
+        'depth' => 1,
+        'echo' => false,
+        'walker' => new \App\Walker\Button_Walker_Nav_Menu()
+    ));
+    set_transient($cache_key, $output, HOUR_IN_SECONDS);
+    return $output;
 }
 
 /**
@@ -73,13 +108,20 @@ function compact_btns_nav() {
  * @return string
  */
 function compact_nav() {
-	return wp_nav_menu([
-		'theme_location' => 'compact_navigation',
-		'menu_class' => 'nav accordion',
-		'depth' => 3,
-		'echo' => false,
-		'walker' => new \App\Walker\Accordion_Walker_Nav_Menu()
-	]);
+    $cache_key = 'compact_nav_' . get_current_blog_id();
+    $cached = get_transient($cache_key);
+    if ($cached !== false) {
+        return $cached;
+    }
+    $output = wp_nav_menu([
+        'theme_location' => 'compact_navigation',
+        'menu_class' => 'nav accordion',
+        'depth' => 3,
+        'echo' => false,
+        'walker' => new \App\Walker\Accordion_Walker_Nav_Menu()
+    ]);
+    set_transient($cache_key, $output, HOUR_IN_SECONDS);
+    return $output;
 }
 
 /**
@@ -88,12 +130,19 @@ function compact_nav() {
  * @return string
  */
 function footer_nav() {
-    return wp_nav_menu([
+    $cache_key = 'footer_nav_' . get_current_blog_id();
+    $cached = get_transient($cache_key);
+    if ($cached !== false) {
+        return $cached;
+    }
+    $output = wp_nav_menu([
         'theme_location' => 'footer_navigation',
         'menu_class' => 'nav vertical',
         'depth' => 1,
         'echo' => false,
     ]);
+    set_transient($cache_key, $output, HOUR_IN_SECONDS);
+    return $output;
 }
 
 /**
@@ -102,11 +151,18 @@ function footer_nav() {
  * @return string
  */
 function privacy_nav() {
-    return wp_nav_menu([
+    $cache_key = 'privacy_nav_' . get_current_blog_id();
+    $cached = get_transient($cache_key);
+    if ($cached !== false) {
+        return $cached;
+    }
+    $output = wp_nav_menu([
         'theme_location' => 'privacy_navigation',
         'menu_class' => 'nav',
         'depth' => 1,
         'echo' => false,
     ]);
+    set_transient($cache_key, $output, HOUR_IN_SECONDS);
+    return $output;
 }
 
